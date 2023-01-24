1723fdbee9fdb0e4aa23dbef9b1ca4b09688014c
Jacob Woodthorpe <jacobwoodthorpe@gmail.com>
2023-01-24 23:43:18 +0000

    Navigation logic issues resolved where Random dino would persist after each game, not changing and play again feature was unreachable.

--- Ark-hangman.py
+++ Ark-hangman.py
@@ -3,10 +3,6 @@ import csv
 import random
 import json
 
-
-
-players_list = []
-
 class Player:
     def __init__(self, username, score = 0, games_won = 0, games_lost = 0):
         self.username = username
@@ -16,9 +12,10 @@ class Player:
         self.player_dict = {
             "username":self.username, "score":self.score, "games_won": self.games_won, "games_lost": self.games_lost
             }
-        players_list.append(self.player_dict)
+        
         
 def find_data():
+    # Reads CSV file and stores each line in the data list
     players = open("players.csv")
     players_dict_file = csv.DictReader(players)
     data = []
@@ -29,12 +26,17 @@ def find_data():
 data = find_data()
 
 def create_player(username):
+    # creates a player dict using Player class and the writes it to the CSV file
+    global player
     player = Player(username)
     with open("players.csv", "a", newline="") as f:
         headersCSV = [i for i in player.player_dict.keys()]
         entries = csv.DictWriter(f, fieldnames=headersCSV)
         entries.writerow(player.player_dict)
         f.close()
+
+def update_stats():
+    pass
 
 def player_stats(username, data):
     # finds player data and returns it
@@ -47,7 +49,13 @@ def player_stats(username, data):
             break
     return player_data
 
+def end():
+    # exit out of program
+    print("\nGood Bye, thank you for playing.")
+    exit()
+
 def random_word():
+    # finds a random word from the dinos.txt file
     with open("dinos.txt", "r") as f:
         dinos = f.readlines()
         word = dinos[random.randrange(0, len(dinos))]
@@ -55,70 +63,56 @@ def random_word():
 
 def hangman(dino):
     # main logic of game
-    play = True
-    while play == True:
-        incorrect_guesses = 0
-        with open("hangman.json", "r") as f:
-            figure = json.load(f)
-
-        win = False
-        print("\nLets Play Hangman!\n")
-        board = ["_"]*len(dino)
-        letters = list(dino)
-        
-        # replaces spaces in the board with actual spaces and ticks them off
-        space = " "
-        if space in board:
-            space_index = letters.index(space)
-            board[space_index] = space
-            letters[space_index] = "%"
-        print((" ".join(board)))
-
-        # main game loop
-        while incorrect_guesses < len(figure):
-            guess = input("\nGuess a letter: ")
-            if guess in letters:
-                while guess in letters:
-                    guess_index = letters.index(guess)
-                    board[guess_index] = guess
-                    letters[guess_index] = "%"
-
-            elif guess == "end":
-                break
-
-            else:
-                incorrect_guesses += 1
-                # prints hangman image
-                print("\n\n\n '{letter}' was not correct!".format(letter=guess))
-                print("\n{image}".format(image=figure[str(incorrect_guesses)]))
-            
-            print((" ".join(board)))
-
-            if "_" not in board or guess == dino:
-                print("\nYou win! It was:\n {dino}!\n".format(dino=dino.title()))
-                win = True
-
-        if not win:
-            print("\nGame Over! Word was: \n{dino}\n".format(dino=dino.title()))
-
-        again = input("\nPlay Again?(y/n): ")
-        if again == "n":
-            play = False
-            end()
+    current_score = 0
+    incorrect_guesses = 0
+    with open("hangman.json", "r") as f:
+        figure = json.load(f)
+    win = False
+    print("\nLets Play Hangman!\n")
+    board = ["_"]*len(dino)
+    letters = list(dino)
+    
+    # replaces spaces in the board with actual spaces and ticks them off
+    space = " "
+    if space in board:
+        space_index = letters.index(space)
+        board[space_index] = space
+        letters[space_index] = "%"
+    print((" ".join(board)))
+    # main game loop
+    while incorrect_guesses < len(figure):
+        guess = input("\nGuess a letter: ")
+        if guess in letters:
+            while guess in letters:
+                guess_index = letters.index(guess)
+                board[guess_index] = guess
+                letters[guess_index] = "%"
+                current_score += 10
+        elif guess == "end":
+            break
         else:
-            continue
-
-
-
-
-def end():
-    # exit out of program
-    print("\nGood Bye, thank you for playing.")
-    exit()
+            incorrect_guesses += 1
+            # prints hangman image
+            print("\n\n\n '{letter}' was not correct!".format(letter=guess))
+            print("\n{image}".format(image=figure[str(incorrect_guesses)]))
+        
+        print((" ".join(board)))
+        print("Score:",current_score)
+        if "_" not in board or guess == dino:
+            print("\nYou win! It was:\n {dino}!\n".format(dino=dino.title()))
+            win = True
+            current_score += 120-(incorrect_guesses*10)
+            print("Score:",current_score)
+            return
+            
+    if not win:
+        print("\nGame Over! Word was: \n{dino}\n".format(dino=dino.title()))
+        return
+    
 
 def main():
     # the loop that the game resides and general menu statements
-    while True:
+    while True:        
         play = input("Play Game?(y/n): ")
         play.lower()
         if play == "y":
@@ -139,10 +133,16 @@ def main():
                         print("---------\nScore: {score}\nWins: {wins}\nLost: {lost}\n---------".format(
                             score=player_data[0], wins=player_data[1], lost=player_data[2]
                             ))
-                        
-                        hangman(dino=random_word().lower())
+                        play = True
+                        while play == True:
+                            hangman(dino=random_word().lower())
+                            again = input("\nPlay Again?(y/n): ")
+                            if again == "n":
+                                play = False
+                                end()
+                            else:
+                                continue
                             
-
             elif option == 2:
                 create = True
                 while create == True:
@@ -154,7 +154,15 @@ def main():
                         create_player(username)
                         create = False
                         print("\nWelcome to hangman, {name}.".format(name=username))
-                        hangman(dino=random_word().lower())
+                        play = True
+                        while play == True:
+                            hangman(dino=random_word().lower())
+                            again = input("\nPlay Again?(y/n): ")
+                            if again == "n":
+                                play = False
+                                end()
+                            else:
+                                continue
 
                     else:
                         print("\nUsername already taken.")
